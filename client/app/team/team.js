a3559777356d6aefac20b58623878f7deb927fff
Atte Saarinen <atte.saarinen@vincit.fi>
2015-04-15 01:05:45 +0300

    Basic team listing functionality with some dummy data

--- client/app/team/team.js
+++ client/app/team/team.js
@@ -8,7 +8,7 @@ m.config(function ($stateProvider) {
       url: 'teams',
       views: {
         'main@': {
-          templateUrl: 'team/team-profile.html',
+          templateUrl: 'team/team-list.html',
           controller: 'TeamListController'
         }
       }
@@ -16,7 +16,7 @@ m.config(function ($stateProvider) {
 
   $stateProvider
     .state('kynarilaarnio.team.profile', {
-      url: 'teams/:id',
+      url: '/:id',
       views: {
         'main@': {
           templateUrl: 'team/team-profile.html',
@@ -27,7 +27,68 @@ m.config(function ($stateProvider) {
 });
 
 m.controller('TeamListController', function ($scope) {
+  $scope.teams = [
+    {
+      id: 1,
+      name: 'Tommi Sotkan Penis'
+    },
+    {
+      id: 2,
+      name: 'Fuksijoukkue'
+    },
+    {
+      id: 3,
+      name: 'TiTe 2'
+    },
+    {
+      id: 4,
+      name: 'TiK 1'
+    },
+    {
+      id: 5,
+      name: 'Cluster 1'
+    },
+    {
+      id: 6,
+      name: 'Oulu :ASd'
+    },
+    {
+      id: 7,
+      name: 'Digit 2'
+    },
+    {
+      id: 8,
+      name: 'random'
+    },
+    {
+      id: 9,
+      name: 'Digit 1'
+    },
+  ];
 });
 
 m.controller('TeamProfileController', function ($scope) {
+});
+
+m.directive('team', function () {
+  return {
+    restrict: 'E',
+    template: '<ng-include src="getTemplateUrl()"></ng-include>',
+    scope: {
+      template: '@',
+      content: '='
+    },
+    link: function (scope) {
+      scope.getTemplateUrl = function () {
+        switch (scope.template) {
+          case 'mini':
+            return 'team/team-mini.html';
+          case 'large':
+            return 'team/team-large.html';
+          case 'profile':
+            return 'team/team-profile.html';
+        }
+      }
+    }
+  }
 });
