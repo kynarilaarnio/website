741ede54de51ab0c81c06a8b48b4580f26ca22c3
Atte Saarinen <atte.saarinen@vincit.fi>
2015-05-29 21:18:10 +0300

    Some profile editing stuff, not fully functional

--- client/app/player/player.js
+++ client/app/player/player.js
@@ -21,6 +21,21 @@ m.config(function ($stateProvider) {
           return Users.get({ id: $stateParams.id });
         }
       }
+    })
+
+    .state('kynarilaarnio.player.profile.edit', {
+      url: '/edit',
+      views: {
+        'main@': {
+          templateUrl: 'player/player-profile-edit.html',
+          controller: 'PlayerProfileEditController'
+        }
+      },
+      resolve: {
+        player: function (Users, $stateParams) {
+          return Users.get({ id: $stateParams.id });
+        }
+      }
     });
 });
 
@@ -28,6 +43,18 @@ m.controller('PlayerProfileController', function ($scope, player) {
   $scope.player = player;
 });
 
-m.controller('PlayerProfileEditController', function ($scope) {
+m.controller('PlayerProfileEditController', function ($scope, $state, Users, Teams, player) {
+  $scope.player = player;
 
+  $scope.saveProfile = function () {
+    Users.update($scope.player).then(function (response) {
+      $state.go('kynarilaarnio.profile', { id: response.id });
+    });
+  };
+
+  $scope.saveTeam = function () {
+    Teams.update($scope.player.team).then(function (response) {
+      $state.go('kynarilaarnio.profile', { id: $scope.player.id });
+    });
+  };
 });
--- client/app/registration/registration.js
+++ client/app/registration/registration.js
@@ -19,10 +19,12 @@ m.controller('RegistrationController', function ($scope, $http, $state) {
   $scope.invitationCode = '';
   $scope.error = false;
 
+  console.log($state.get());
+
   $scope.register = function () {
     $http.post('/api/register', { code: $scope.invitationCode })
       .success(function (response) {
-        $state.go('kynarilaarnio.player.profile', { id: response.id });
+        $state.go('kynarilaarnio.player.profile.edit', { id: response.id });
       })
       .error(function (response) {
         $scope.error = true;
