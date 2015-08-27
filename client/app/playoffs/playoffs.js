99bc88448a9c055c167ead7595b1a3d8624b2300
Miro Nieminen <miro.nieminen@futurice.com>
2015-08-27 23:00:03 +0300

    Add basic version of the playoff brackets

--- client/app/playoffs/playoffs.js
+++ client/app/playoffs/playoffs.js
@@ -17,4 +17,26 @@ m.config(function ($stateProvider) {
 
 m.controller('PlayoffsController', function ($scope) {
   $scope.keke = 'test';
+
+  // TODO: this would be cool.. see template. But the extra divs that
+  // ng-repeat generates breaks flexbox
+  $scope.rounds = [
+    {
+      roundNumber: 1,
+      matchPairs: [1,2,3,4,5,6,7,8]
+    },
+    {
+      roundNumber: 2,
+      matchPairs: [9,10,11,12]
+    },
+    {
+      roundNumber: 3,
+      matchPairs: [13,14]
+    },
+    {
+      roundNumber: 4,
+      matchPairs: [15]
+    }
+
+  ];
 });
